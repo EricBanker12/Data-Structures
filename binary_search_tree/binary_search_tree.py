776595efdd3c4cd237b5cff25f326666bf89519e
ericbanker12 <ericbanker12@gmail.com>
2020-02-25 17:01:16 -0600

    clean up unnecessary comparison in binary_search_tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -16,12 +16,12 @@ class BinarySearchTree:
     # Insert the given value into the tree
     def insert(self, value):
         if value >= self.value:
-            if self.right != None:
+            if self.right:
                 self.right.insert(value)
             else:
                 self.right = BinarySearchTree(value)
         if value < self.value:
-            if self.left != None:
+            if self.left:
                 self.left.insert(value)
             else:
                 self.left = BinarySearchTree(value)
@@ -31,15 +31,15 @@ class BinarySearchTree:
     def contains(self, target):
         if target == self.value:
             return True
-        if target > self.value and self.right != None:
+        if target > self.value and self.right:
             return self.right.contains(target)
-        if target < self.value and self.left != None:
+        if target < self.value and self.left:
             return self.left.contains(target)
         return False
 
     # Return the maximum value found in the tree
     def get_max(self):
-        if self.right != None:
+        if self.right:
             return self.right.get_max()
         else:
             return self.value
@@ -48,9 +48,9 @@ class BinarySearchTree:
     # You may use a recursive or iterative approach
     def for_each(self, cb):
         cb(self.value)
-        if self.left != None:
+        if self.left:
             self.left.for_each(cb)
-        if self.right != None:
+        if self.right:
             self.right.for_each(cb)
 
     # DAY 2 Project -----------------------
@@ -58,10 +58,10 @@ class BinarySearchTree:
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, *args):
-        if self.left != None:
+        if self.left:
             self.left.in_order_print()
         print(self.value)
-        if self.right != None:
+        if self.right:
             self.right.in_order_print()
 
     # Print the value of every node, starting with the given node,
@@ -72,9 +72,9 @@ class BinarySearchTree:
         while q.len():
             node = q.dequeue()
             print(node.value)
-            if node.left != None:
+            if node.left:
                 q.enqueue(node.left)
-            if node.right != None:
+            if node.right:
                 q.enqueue(node.right)
 
     # Print the value of every node, starting with the given node,
@@ -85,9 +85,9 @@ class BinarySearchTree:
         while s.len():
             node = s.pop()
             print(node.value)
-            if node.left != None:
+            if node.left:
                 s.push(node.left)
-            if node.right != None:
+            if node.right:
                 s.push(node.right)
 
     # STRETCH Goals -------------------------
@@ -96,15 +96,15 @@ class BinarySearchTree:
     # Print Pre-order recursive DFT
     def pre_order_dft(self, *args):
         print(self.value)
-        if self.left != None:
+        if self.left:
             self.left.pre_order_dft()
-        if self.right != None:
+        if self.right:
             self.right.pre_order_dft()
 
     # Print Post-order recursive DFT
     def post_order_dft(self, *args):
-        if self.left != None:
+        if self.left:
             self.left.post_order_dft()
-        if self.right != None:
+        if self.right:
             self.right.post_order_dft()
         print(self.value)
