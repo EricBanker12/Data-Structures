3b22cba82e4b269705764f7a6f607080a00b907d
ericbanker12 <ericbanker12@gmail.com>
2020-02-25 14:35:16 -0600

    complete binary_search_tree part 1

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,8 +1,11 @@
+import os
 import sys
-sys.path.append('../queue_and_stack')
+
+queue_stack_path = os.path.normpath(os.path.join(__file__, '../../queue_and_stack'))
+sys.path.append(queue_stack_path)
+
 from dll_queue import Queue
 from dll_stack import Stack
-
 
 class BinarySearchTree:
     def __init__(self, value):
@@ -12,21 +15,43 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value >= self.value:
+            if self.right != None:
+                self.right.insert(value)
+            else:
+                self.right = BinarySearchTree(value)
+        if value < self.value:
+            if self.left != None:
+                self.left.insert(value)
+            else:
+                self.left = BinarySearchTree(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if target == self.value:
+            return True
+        if target > self.value and self.right != None:
+            return self.right.contains(target)
+        if target < self.value and self.left != None:
+            return self.left.contains(target)
+        return False
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if self.right != None:
+            return self.right.get_max()
+        else:
+            return self.value
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        cb(self.value)
+        if self.left != None:
+            self.left.for_each(cb)
+        if self.right != None:
+            self.right.for_each(cb)
 
     # DAY 2 Project -----------------------
 
